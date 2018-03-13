ba9b40e46360f3d28b7a2dfd28d842655acf7f92
yoyo89757001 <332663557@qq.com>
2018-03-13 10:19:54 +0800

    Initial committt

---
+++ app/src/main/java/com/example/aofeitv/ui/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.example.aofeitv.ui;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.aofeitv.R;
+import com.example.aofeitv.utils.Utils;
+
+public class BaseActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
